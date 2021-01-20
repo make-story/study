9998f5adfae4b1c33ff4dccab8a8267c474a77aa
Sung-min Yu <yu9221@gmail.com>
2021-01-20 12:18:30 +0900

    update

--- javascript/array.js
+++ javascript/array.js
@@ -46,6 +46,16 @@ fruits.push("망고");
 var pos = fruits.indexOf("바나나");
 // 1
 
+const array1 = [5, 12, 8, 130, 44];
+const found = array1.find(element => element > 10);
+console.log(found);
+// expected output: 12
+
+const array1 = [5, 12, 8, 130, 44];
+const isLargeNumber = (element) => element > 13;
+console.log(array1.findIndex(isLargeNumber));
+// expected output: 3
+
 
 
 // 배열 복사하기
