e378494b7207dd4cf5bf48682cb1382b96f55c75
Sung-min Yu <yu9221@gmail.com>
2021-05-21 21:07:16 +0900

    update

--- javascript/is.js
+++ javascript/is.js
@@ -1,6 +1,17 @@
 
 
+// 정규식 활용 OR
 let test = 'test';
 if(/^string|number/.test(typeof test)) {
     console.log('OK!');
 }
+
+
+// object {} 활용
+const getVaule = (value) => {
+    return {
+        [value === 0] : 100,
+        [value === 'test'] : 'ysm',
+        [typeof value === 'object'] : (value.test = 'test'),
+    }.test;
+};
