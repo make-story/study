922ece9856022d9e0a9c0a1f693346d3a7925698
Sung-min Yu <yu9221@gmail.com>
2021-05-23 21:59:29 +0900

    update

--- javascript/sort.js
+++ javascript/sort.js
@@ -45,6 +45,7 @@ function bubbleSort(data) {
 	return 0;
 });
 	
+
 // 정렬
 let list = [1, 6, -3, 2, 45, 2].sort((current, next) => {
 	if(current > next) {
@@ -54,6 +55,35 @@ let list = [1, 6, -3, 2, 45, 2].sort((current, next) => {
 	}
 	return 0;
 });
+var score = [4, 11, 2, 10, 3, 1]; 
+score.sort(function(a, b) { // 오름차순
+    return a - b;
+    // 1, 2, 3, 4, 10, 11
+});
+
+score.sort(function(a, b) { // 내림차순
+    return b - a;
+    // 11, 10, 4, 3, 2, 1
+});
+
+
+// 이름순으로 정렬 
+var student = [
+    { name : "재석", age : 21},
+    { name : "광희", age : 25},
+    { name : "형돈", age : 13},
+    { name : "명수", age : 44}
+]
+student.sort(function(a, b) { // 오름차순
+    return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+    // 광희, 명수, 재석, 형돈
+});
+
+student.sort(function(a, b) { // 내림차순
+    return a.name > b.name ? -1 : a.name < b.name ? 1 : 0;
+    // 형돈, 재석, 명수, 광희
+});
+
 
 // 뒤에서 하나빼고, 앞에서 하나 뺀다.
 let arr = [];
