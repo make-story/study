a5f14ab633bfd2bb52f3306585e3900e0ec6f5a4
Sung-min Yu <yu9221@gmail.com>
2022-03-27 00:10:43 +0900

    update

--- 인프라/서버/NodeJS/file.js
+++ 인프라/서버/NodeJS/file.js
@@ -2,7 +2,17 @@
 // 경로
 const source = path.resolve(__dirname, '../');
 const read = fs.readdirSync(source, { withFileTypes: true });
+const { directory, file } = read.reduce((accumulator, item, index) => {
+    if(item.isDirectory()) {
+        // 폴더 리스트
+        accumulator.directory.push(item.name);
+    }else {
+        // 파일 리스트
+        accumulator.file.push(item.name);
+    }
+    return accumulator;
+}, { directory: [], file: [] });
 // 폴더 리스트
-const directory = read.filter(dirent => dirent.isDirectory()).map(dirent => dirent.name);
+//const directory = read.filter(dirent => dirent.isDirectory()).map(dirent => dirent.name);
 // 파일 리스트
-const file = read.filter(dirent => !dirent.isDirectory()).map(dirent => dirent.name);
+//const file = read.filter(dirent => !dirent.isDirectory()).map(dirent => dirent.name);
