c117822bf1f0d3718433709399246bbea41d0d81
Sung-min Yu <yu9221@gmail.com>
2022-04-15 23:08:57 +0900

    update

--- 프론트/JavaScript/class.js
+++ 프론트/JavaScript/class.js
@@ -31,11 +31,15 @@ var s4 = new School("저학교");
 console.log(s1.printName == s2.printName); // true
 console.log(s3.printName == s4.printName); // false 
 
+// ---------- ----------
 
 // 클래스는 호이스팅 안된다!
 
+// ---------- ----------
 
-// 클래스 선언
+/**
+ * 클래스 선언
+ */
 class Student {
 	constructor(name) {
 		this.name = name;
@@ -51,9 +55,11 @@ function School(name) {
 var s2 = new School("학교");
 console.log(s2.name); // 학교 
 
+// ---------- ----------
 
-
-// get/set 메소드 (getter / setter)
+/**
+ * get/set 메소드 (getter / setter)
+ */
 class Person {
 	constructor(name) {
 		this._name = name;
@@ -75,9 +81,11 @@ console.log(p.name); // 성민
 p.name = "진은";
 console.log(p.name); // 진은 
 
+// ---------- ----------
 
-
-// 정적메소드 (static)
+/**
+ * 정적메소드 (static)
+ */
 class Student {
 	constructor(name) {
 		this.name = name;
@@ -107,8 +115,12 @@ var name = Student.findName(s);
 
 console.log(name); // 성민 
 
+// ---------- ----------
 
-// 상속
+/**
+ * 상속
+ * https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Classes#extends%EB%A5%BC_%ED%86%B5%ED%95%9C_%ED%81%B4%EB%9E%98%EC%8A%A4_%EC%83%81%EC%86%8Dsub_classing
+ */
 function A(a) {
 	this.a = a;
 }
@@ -118,7 +130,8 @@ A.prototype.printA = function() {
 
 class B extends A {
 	constructor(a, b) {
-		super(a);
+		// subclass에 constructor가 있다면, "this"를 사용하기 전에 가장 먼저 super()를 호출해야 합니다.
+		super(a); // super class 생성자를 호출하여 a 매개변수 전달
 		this.b = b;
 	}
 
@@ -153,10 +166,65 @@ obj.printAll();
 
 C.sayHello(); // 안녕하세요. 
 
+// ---------- ----------
 
+/**
+ * super 를 통한 상위 클래스 호출
+ * https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Classes#super_%EB%A5%BC_%ED%86%B5%ED%95%9C_%EC%83%81%EC%9C%84_%ED%81%B4%EB%9E%98%EC%8A%A4_%ED%98%B8%EC%B6%9C
+ */
+ class Cat {
+	constructor(name) {
+		this.name = name;
+	}
+  
+	speak() {
+		console.log(`${this.name} makes a noise.`);
+	}
+}
+  
+class Lion extends Cat {
+	speak() {
+		super.speak();
+		console.log(`${this.name} roars.`);
+	}
+}
+  
+let l = new Lion('Fuzzy');
+l.speak();
+// Fuzzy makes a noise.
+// Fuzzy roars.
 
-// Private (프라이빗)
-class ClassWithPrivateField {
+// ---------- ----------
+
+/**
+ * Field 선언
+ * https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Classes#field_%EC%84%A0%EC%96%B8
+ */
+class Rectangle {
+	height = 0;
+	width;
+	constructor(height, width) {
+	  this.height = height;
+	  this.width = width;
+	}
+}
+
+// Private 필드 선언
+class Rectangle {
+	#height = 0;
+	#width;
+	constructor(height, width) {
+		this.#height = height;
+		this.#width = width;
+	}
+}
+
+// ---------- ----------
+
+/**
+ * Private (프라이빗)
+ */
+ class ClassWithPrivateField {
 	#privateField
 }
   
