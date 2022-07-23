66ef431b65da6ac4e764403d207e88d09dbcef37
Sung-min Yu <yu9221@gmail.com>
2022-07-23 15:20:16 +0900

    update

--- 프론트/Code/JavaScript/sort.js
+++ 프론트/Code/JavaScript/sort.js
@@ -1,17 +1,26 @@
+/**
+ * 배열 정렬
+ * array.sort(), array.reverse()
+ */
+const list3 = [1, 2, 11, 34, 22];
+list3.sort((a, b) => a - b);
+// a - b: 오름차순 정렬
+// b - a: 내림차순 정렬
+// a = b: 순서를 변경하지 않음
 
 // 소수 구하기
 // https://velog.io/@dosanahnchangho/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%86%8C%EC%88%98-%EA%B5%AC%ED%95%98%EA%B8%B0-JavaScript
 // ** 연산 : 거듭제곱 (https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Operators/Assignment_Operators#Exponentiation_assignment)
 
-// ---------- ---------- ---------- ---------- ---------- 
+// ---------- ---------- ---------- ---------- ----------
 // 데이터 교환
 // index 1의 값을 index 2에 넣고, index2의 값을 index1에 넣는다.
 function swap(arr, index1, index2) {
-	var temp = arr[index1];
-	arr[index1] = arr[index2];
-	arr[index2] = temp;
+  var temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
 }
-// ---------- ---------- ---------- ---------- ---------- 
+// ---------- ---------- ---------- ---------- ----------
 
 /*
 -
@@ -21,85 +30,86 @@ function swap(arr, index1, index2) {
 따라서 배열의 데이터를 여러 번 반복적으로 탐색하면서 인접한 값을 서로 비교해 왼쪽 값이 오른쪽 값보다 크다면 두 값을 서로 바꿔야 한다.
 */
 function bubbleSort(data) {
-	var numElement = data.length;
-	var temp;
+  var numElement = data.length;
+  var temp;
 
-	// 큰값을 뒤로 보낸다.
-	// 배열의 끝에서 부터 정렬을 완성해 간다.
-	for(var outer=numElement; outer>=2; --outer) { // 전체 배열 크기에서 작아지며 검사
-		for(var inner=0; inner<=outer-1; ++inner) { // 첫번째 배열부터 비교해 간다.
-			// 비교 index 순서: 0 <-> 1 비교, 1 <-> 2 비교, 2 <-> 3 비교, 3 <-> 4 비교 ...
-			if(data[inner] > data[inner+1]) {
-				swap(data, inner, inner+1);
-			}
-		}
-	}
-} 
+  // 큰값을 뒤로 보낸다.
+  // 배열의 끝에서 부터 정렬을 완성해 간다.
+  for (var outer = numElement; outer >= 2; --outer) {
+    // 전체 배열 크기에서 작아지며 검사
+    for (var inner = 0; inner <= outer - 1; ++inner) {
+      // 첫번째 배열부터 비교해 간다.
+      // 비교 index 순서: 0 <-> 1 비교, 1 <-> 2 비교, 2 <-> 3 비교, 3 <-> 4 비교 ...
+      if (data[inner] > data[inner + 1]) {
+        swap(data, inner, inner + 1);
+      }
+    }
+  }
+}
 
-[].sort(function(current, next) {
-	if(current > next) {
-		return -1;
-	}else if(current < next) {
-		return 1;
-	}
-	return 0;
+[].sort(function (current, next) {
+  if (current > next) {
+    return -1;
+  } else if (current < next) {
+    return 1;
+  }
+  return 0;
 });
-	
 
 // 정렬
 let list = [1, 6, -3, 2, 45, 2].sort((current, next) => {
-	if(current > next) {
-		return 1;
-	}else if(current < next) {
-		return -1;
-	}
-	return 0;
+  if (current > next) {
+    return 1;
+  } else if (current < next) {
+    return -1;
+  }
+  return 0;
 });
-var score = [4, 11, 2, 10, 3, 1]; 
-score.sort(function(a, b) { // 오름차순
-    return a - b;
-    // 1, 2, 3, 4, 10, 11
-});
-
-score.sort(function(a, b) { // 내림차순
-    return b - a;
-    // 11, 10, 4, 3, 2, 1
+var score = [4, 11, 2, 10, 3, 1];
+score.sort(function (a, b) {
+  // 오름차순
+  return a - b;
+  // 1, 2, 3, 4, 10, 11
 });
 
+score.sort(function (a, b) {
+  // 내림차순
+  return b - a;
+  // 11, 10, 4, 3, 2, 1
+});
 
-// 이름순으로 정렬 
+// 이름순으로 정렬
 var student = [
-    { name : "재석", age : 21},
-    { name : "광희", age : 25},
-    { name : "형돈", age : 13},
-    { name : "명수", age : 44}
-]
-student.sort(function(a, b) { // 오름차순
-    return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
-    // 광희, 명수, 재석, 형돈
+  { name: '재석', age: 21 },
+  { name: '광희', age: 25 },
+  { name: '형돈', age: 13 },
+  { name: '명수', age: 44 },
+];
+student.sort(function (a, b) {
+  // 오름차순
+  return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+  // 광희, 명수, 재석, 형돈
 });
 
-student.sort(function(a, b) { // 내림차순
-    return a.name > b.name ? -1 : a.name < b.name ? 1 : 0;
-    // 형돈, 재석, 명수, 광희
+student.sort(function (a, b) {
+  // 내림차순
+  return a.name > b.name ? -1 : a.name < b.name ? 1 : 0;
+  // 형돈, 재석, 명수, 광희
 });
-
 
 // 뒤에서 하나빼고, 앞에서 하나 뺀다.
 let arr = [];
-while(list.length) {
-	// pop
-	if(list.length) {
-		arr.push(list.pop());
-	}
-	// shift
-	if(list.length) {
-		arr.push(list.shift());
-	}
+while (list.length) {
+  // pop
+  if (list.length) {
+    arr.push(list.pop());
+  }
+  // shift
+  if (list.length) {
+    arr.push(list.shift());
+  }
 }
 console.log(arr);
-
-
 
 /*
 -
@@ -109,24 +119,23 @@ console.log(arr);
 다음에는 두 번째 자리에 올 요소를 정렬하는 식이다.
 */
 function selectionSort(data) {
-	var min; // 비교해 가며 최소값에 해당하는 index
+  var min; // 비교해 가며 최소값에 해당하는 index
 
-	// 작은값을 앞으로 보낸다.
-	// 배열의 앞부터 정렬해가며 완성한다.
-	for(var outer=0; outer<=data.length-2; ++outer) {
-		min = outer;
-		console.log('min: ' + min);
-		for(var inner=outer+1; inner<=data.length-1; ++inner) { // 첫번째 인덱스 다음부터 비교해간다.
-			console.log('inner: ' + inner);
-			if(data[inner] < data[min]) {
-				min = inner;
-			}
-		}
-		swap(data, outer, min);
-	}
-} 
-
-
+  // 작은값을 앞으로 보낸다.
+  // 배열의 앞부터 정렬해가며 완성한다.
+  for (var outer = 0; outer <= data.length - 2; ++outer) {
+    min = outer;
+    console.log('min: ' + min);
+    for (var inner = outer + 1; inner <= data.length - 1; ++inner) {
+      // 첫번째 인덱스 다음부터 비교해간다.
+      console.log('inner: ' + inner);
+      if (data[inner] < data[min]) {
+        min = inner;
+      }
+    }
+    swap(data, outer, min);
+  }
+}
 
 /*
 -
@@ -134,31 +143,29 @@ function selectionSort(data) {
 자신의 위치에 요소를 넣어가며 정렬하는 방식
 */
 function insertionSort(data) {
-	var temp;
-	var inner;
+  var temp;
+  var inner;
 
-	// 배열의 두번째 인덱스 부터
-	// 자신의 앞에 있는 값들과 비교하여, 자신이 들어가야 할 곳에 삽입된다.
-	for(var outer=1; outer<=data.length-1; ++outer) {
-		temp = data[outer]; // 값을 임시로 가지고 있다.
-		inner = outer;
+  // 배열의 두번째 인덱스 부터
+  // 자신의 앞에 있는 값들과 비교하여, 자신이 들어가야 할 곳에 삽입된다.
+  for (var outer = 1; outer <= data.length - 1; ++outer) {
+    temp = data[outer]; // 값을 임시로 가지고 있다.
+    inner = outer;
 
-		console.log('outer: ' + outer);
-		console.log('inner: ' + inner);
+    console.log('outer: ' + outer);
+    console.log('inner: ' + inner);
 
-		// 현재 자신의 위치 앞에 자신보다 큰값이 없을 때 까지 검사
-		while(inner > 0 && (data[inner-1] >= temp)) {
-			// 자신의 앞에 있는 값이 자신보다 크면, 그 값을 현재 자신의 위치와 바꾼다.
-			data[inner] = data[inner-1];
-			--inner;
-		}
+    // 현재 자신의 위치 앞에 자신보다 큰값이 없을 때 까지 검사
+    while (inner > 0 && data[inner - 1] >= temp) {
+      // 자신의 앞에 있는 값이 자신보다 크면, 그 값을 현재 자신의 위치와 바꾼다.
+      data[inner] = data[inner - 1];
+      --inner;
+    }
 
-		// 내가 들어가야 할 위치에 삽입된다.
-		data[inner] = temp; 
-	}
-} 
-
-
+    // 내가 들어가야 할 위치에 삽입된다.
+    data[inner] = temp;
+  }
+}
 
 /*
 -
@@ -178,27 +185,26 @@ console.log(a);
 console.log(qSort(a));
 */
 function qSort(arr) {
-	if(arr.length == 0) {
-		return [];
-	}
-	var left = [];
-	var right = [];
-	var pivot = arr[0]; // 왼쪽 배열 오른쪽 배열으로 분류할 비교값(기준)
+  if (arr.length == 0) {
+    return [];
+  }
+  var left = [];
+  var right = [];
+  var pivot = arr[0]; // 왼쪽 배열 오른쪽 배열으로 분류할 비교값(기준)
 
-	// 특정 기분값(pivot) 을 정하고, 그 값보다 작은 값은 왼쪽, 큰값을 오른쪽으로 보내며 정렬한다.
-	// 특정 값을 0번째 index 로 잡고 1번째 인덱스와 비교해가며 검사한다.
-	for(var i=1; i<arr.length; i++) {
-		if(arr[i] < pivot) {
-			// 피벗보다 작은 값은 왼쪽
-			left.push(arr[i]);
-		}else {
-			// 피벗보다 큰 값은 오른쪽
-			right.push(arr[i]);
-		}
-	}
-	return qSort(left).concat(pivot, qSort(right)); // 재귀호출 / 배열합치기
-} 
-
+  // 특정 기분값(pivot) 을 정하고, 그 값보다 작은 값은 왼쪽, 큰값을 오른쪽으로 보내며 정렬한다.
+  // 특정 값을 0번째 index 로 잡고 1번째 인덱스와 비교해가며 검사한다.
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      // 피벗보다 작은 값은 왼쪽
+      left.push(arr[i]);
+    } else {
+      // 피벗보다 큰 값은 오른쪽
+      right.push(arr[i]);
+    }
+  }
+  return qSort(left).concat(pivot, qSort(right)); // 재귀호출 / 배열합치기
+}
 
 /*
 피보나치 숫자 계산
@@ -213,20 +219,18 @@ function qSort(arr) {
 */
 // 문제: 피보나치 수열을 입력 받은 숫자 개수만큼 출력하라
 function fibonacci(n) {
-    if(n < 2) return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
+  if (n < 2) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 var input = 8;
-for(var i=1; i<=input; i++) {
-	fibonacci(i);
-} 
+for (var i = 1; i <= input; i++) {
+  fibonacci(i);
+}
 
-
-// ---------- ---------- ---------- ---------- ---------- 
+// ---------- ---------- ---------- ---------- ----------
 
 // 합병 정렬 (모두 조각낸 후 정렬해 간다.)
 // 셀 정렬
 // 머지 정렬
 // 힙 정렬
-
