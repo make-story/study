c266c1521578022510e8325f004671179e760985
Sung-min Yu <yu9221@gmail.com>
2022-08-14 18:51:37 +0900

    update

---
+++ 프론트/타입스크립트/global.d.ts
@@ -0,0 +1,19 @@
+export declare global {
+  /**
+   * 전역 window 객체
+   */
+  export interface Window {
+    /* SNS 공유 */
+    Kakao: any;
+    kakao: any;
+
+    /*네이버 페이*/
+    naver: any;
+
+    /*ios 앱*/
+    webkit: any;
+
+    /* 카카오챗봇 */
+    Kakaoi: any;
+  }
+}
---
+++ 프론트/타입스크립트/index.d.ts
@@ -0,0 +1,10 @@
+/**
+ * JavaScript 로 작성된 모듈
+ */
+declare module 'react-scroll';
+declare module 'react-slick';
+declare namespace JSX {
+  interface IntrinsicElements {
+    'lottie-player': any;
+  }
+}
