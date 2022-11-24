62071eead4a316b7d023d510283288326c778078
Sung-min Yu <yu9221@gmail.com>
2022-11-25 00:13:00 +0900

    update

--- PWA/serviceworkers/client.js
+++ PWA/serviceworkers/client.js
@@ -23,21 +23,29 @@ const setUnregisterAll = () => {
 };
 
 // 리소스 로드와 서비스워커 등록 후 fetch 가 동시에 일어나 버벅거리지 않도록, load 후 서비스워커 작업을 실행
+// https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
 // https://developers.google.com/web/fundamentals/primers/service-workers/registration
 const load = async () => {
     const registration = await navigator?.serviceWorker?.getRegistration();
     if(registration) {
+        // 서비스워커 등록 취소
+        // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/unregister
         /*setUnregisterAll()
         .then(
             (value) => console.log('서비스워커 전체해제 성공!', value), 
             (error) => console.log('서비스워커 전체해제 실패!', error),
         );*/
+        
+        // 서비스워커 업데이트된 버전이 있는지 서버를 확인
+        // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/update
         registration?.update()
         .then(
             (registration) => console.log('서비스워커 업데이트 성공!', registration?.scope), 
             (error) => console.log('서비스워커 업데이트 실패!', error),
         );
     }else {
+        // 서비스워커 등록 또는 업데이트
+        // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration
         navigator?.serviceWorker?.register(`/serviceworker-workbox.js?time=${new Date().getTime()}`)
         .then(
             (registration) => console.log('서비스워커 등록 성공!', registration?.scope),
