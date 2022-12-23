ccf4004ad00c27f868ac8036bb4d33c7c917d543
sung-min <yu9221@gmail.com>
2022-12-23 12:19:02 +0900

    update

--- 프론트/Code/JavaScript/array.js
+++ 프론트/Code/JavaScript/array.js
@@ -41,6 +41,12 @@ const everyBool = [].every((item, index, list) => {
  */
 var fruits = ['사과', '바나나'];
 
+const clone1 = arr => arr.slice(0);
+const clone2 = arr => [...arr];
+const clone3 = arr => arr.map(x => x);
+const clone4 = arr => JSON.parse(JSON.stringify(arr));
+const clone5 = arr => arr.concat({});
+
 
 // Array.of()
 Array.of(7);       // [7]
