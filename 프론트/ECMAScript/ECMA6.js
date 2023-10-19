d2cb32e47438ea55e1be8e1bb25ae31f42d7b509
sung-min <yu9221@gmail.com>
2023-10-19 18:33:39 +0900

    update

--- 프론트/ECMAScript/ECMA6.js
+++ 프론트/ECMAScript/ECMA6.js
@@ -268,7 +268,7 @@ let object = {
 /*
 - 
 객체 프로퍼티를 새로운 구문으로 생성
-ES6는 객체 리터럴로 프로퍼티를 생성하는 새로운 구문을 제공한다.
+ES6는 객체 리터럴로 프로퍼티를 생성하는 새로운 구문을 제공한다. (메소드 축약 표현)
 */
 // 프로퍼티 정의
 let x = 1, y = 2;
@@ -289,6 +289,41 @@ let object = {
 	["first" + "Name"]: "성민"
 };
 console.log(object["first" + "Name"]); // 성민
+
+
+
+/*
+-
+참고: __proto__ 에 의한 상속
+*/
+// ES5
+var parent = {
+	name: 'parent',
+	sayHi: function() {
+		console.log('Hi! ' + this.name);
+	}
+};
+// 프로토타입 패턴 상속
+var child = Object.create(parent);
+child.name = 'child';
+parent.sayHi(); // Hi! parent
+child.sayHi();  // Hi! child
+
+// ES6
+const parent = {
+	name: 'parent',
+	sayHi() {
+		console.log('Hi! ' + this.name);
+	}
+};
+const child = {
+	// child 객체의 프로토타입 객체에 parent 객체를 바인딩하여 상속을 구현한다.
+	__proto__: parent,
+	name: 'child'
+};
+parent.sayHi(); // Hi! parent
+child.sayHi();  // Hi! child
+
 
 
 
