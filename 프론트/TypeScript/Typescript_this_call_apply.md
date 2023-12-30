aec4e6b4fab580a9f214e2c515d9de5ba664e746
sung-min <yu9221@gmail.com>
2023-12-30 11:30:15 +0900

    update

---
+++ 프론트/TypeScript/Typescript_this_call_apply.md
@@ -0,0 +1,7 @@
+# bind / call / apply
+
+strictBindCallApply
+
+Function 의 내장 함수인 bind / call / apply 를 사용할 때, 엄격하게 체크하도록 하는 옵션
+
+https://medium.com/@2woongjae/typescript-3-2-new-features-d05b66d1230c
---
+++ 프론트/TypeScript/Typescript_유용한_참고_코드.ts
@@ -0,0 +1,21 @@
+/**
+ * 자주 사용하는 타입스크립트 코드
+ */
+
+export const MenuPageCornerId = {
+  MAIN_UPPER: 'M01_main_m.16',
+  MAIN_MIDDLE: 'M01_main_m.21',
+  MAIN_BOTTOM: 'M01_main_m.26',
+  GIFT_MIDDLE: 'M01_giftRecommend_m.1',
+  BRAND_MANUAL_1: 'M01_brandMain_m.1',
+  BRAND_MANUAL_2: 'M01_brandMain_m.2',
+  LIVE: 'M01_main_m.30',
+} as const;
+
+export type MenuPageCornerIdType =
+  (typeof MenuPageCornerId)[keyof typeof MenuPageCornerId];
+
+function test(payload: MenuPageCornerIdType) {
+  console.log(payload);
+}
+test(MenuPageCornerId.LIVE);
